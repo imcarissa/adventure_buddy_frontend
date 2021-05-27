c96eaeb239555935a761ce1c15669a42f75c28b4
imcarissa <carissa.sarreal@gmail.com>
2021-05-27 05:40:24 -0400

    DOMContentLoaded event listener, getAdventures function

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("index.js")
+const endPoint = "http://localhost:3000/api/v1/adventures"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getAdventures()
+})
+
+function getAdventures() {
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(adventures => {
+      adventures.data.forEach(adventure => {
+        const adventureMarkup = `
+          <div data-id=${adventure.id}>
+            <img src=${adventure.attributes.image_url} height="200" width="250">
+            <h3>${adventure.attributes.title}</h3>
+            <p>${adventure.attributes.category.name}</p>
+            <button data-id=${adventure.id}>edit</button>
+          </div>
+          <br><br>`;
+
+          document.querySelector('#adventure-container').innerHTML += adventureMarkup
+      })
+    })
+}

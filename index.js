994c240c2585a2bec6449ca299a7742f736d74a8
imcarissa <carissa.sarreal@gmail.com>
2021-05-31 08:33:51 -0400

    function render() created to make DRY HTML

--- index.js
+++ index.js
@@ -14,21 +14,25 @@ function getAdventures() {
     .then(response => response.json())
     .then(adventures => {
       adventures.data.forEach(adventure => {
-        
-        const adventureMarkup = `
-          <div data-id=${adventure.id}>
-            <img src=${adventure.attributes.image_url} height="200" width="250">
-            <h3>${adventure.attributes.title}</h3>
-            <p><strong>Location:</strong> ${adventure.attributes.location}</p>
-            <p><strong>Description:</strong> ${adventure.attributes.description}</p>
-            <p><strong>Category:</strong> ${adventure.attributes.category.name}</p>
-            <button data-id=${adventure.id}>edit</button>
-          </div>
-          <br><br>`;
-
-          document.querySelector('#adventure-container').innerHTML += adventureMarkup
+        render(adventure)
       })
     })
+}
+
+
+function render() {
+  const adventureMarkup = `
+    <div data-id=${adventure.id}>
+      <img src=${adventure.attributes.image_url} height="200" width="250">
+      <h3>${adventure.attributes.title}</h3>
+      <p><strong>Location:</strong> ${adventure.attributes.location}</p>
+      <p><strong>Description:</strong> ${adventure.attributes.description}</p>
+      <p><strong>Category:</strong> ${adventure.attributes.category.name}</p>
+      <button data-id=${adventure.id}>edit</button>
+    </div>
+    <br><br>`;
+
+    document.querySelector('#adventure-container').innerHTML += adventureMarkup
 }
 
 

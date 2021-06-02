23f267f21fb6179482cca8647a10cbb8b9c36c91
imcarissa <carissa.sarreal@gmail.com>
2021-06-02 01:14:14 -0400

    constructor created

--- src/adventure.js
+++ src/adventure.js
@@ -1,3 +1,11 @@
 class Adventure {
     
+    constructor(data) {
+        this.id = data.id
+        this.title = data.title
+        this.location = data.location
+        this.description = data.description
+        this.image_url = data.image_url
+        this.category = data.category
+    }
 }

b5063a5df72b3aadbef15d6d28594dcc803490e4
imcarissa <carissa.sarreal@gmail.com>
2021-06-02 10:04:30 -0400

    adventure class finished

--- src/adventure.js
+++ src/adventure.js
@@ -2,11 +2,13 @@ class Adventure {
 
     constructor(adventure, adventureAttributes) {
         this.id = adventure.id
-        debugger
         this.title = adventureAttributes.title
         this.location = adventureAttributes.location
         this.description = adventureAttributes.description
         this.image_url = adventureAttributes.image_url
         this.category = adventureAttributes.category
+        Adventure.all.push(this)
     }
 }
+
+Adventure.all = [];
